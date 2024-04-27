2ae7abe9b0c40c9cdbaf07ffeffeb847b0144d9f
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2024-04-27 21:12:37 +0300

    Init backend

--- todo-backend/Controllers/WeatherForecastController.cs
+++ todo-backend/Controllers/WeatherForecastController.cs
@@ -27,6 +27,7 @@ namespace todo_backend.Controllers
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
+
             .ToArray();
         }
     }
