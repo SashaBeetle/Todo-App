b57858a72a67077fdcf24355ee58da4f6a6fee7e
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2024-05-02 17:22:26 +0300

    Add Tailwind.

---
+++ todo-frontend/src/components/card-component/card-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-component.component.html',
+  styleUrl: './card-component.component.scss'
+})
+export class CardComponentComponent {
+
+}
