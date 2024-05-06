7a1dab0e2e4582709144956029671e8decf659a2
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2024-05-06 14:52:29 +0300

    Add add-list component

---
+++ todo-frontend/src/components/add-list/add-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-list.component.html',
+  styleUrl: './add-list.component.scss'
+})
+export class AddListComponent {
+
+}
--- todo-frontend/src/components/list-component/list-component.component.ts
+++ todo-frontend/src/components/list-component/list-component.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { CardComponentComponent } from '../card-component/card-component.component';
+import { AddListComponent } from '../add-list/add-list.component';
 
 @Component({
   selector: 'app-list-component',
   standalone: true,
   imports: [
     CardComponentComponent,
+    AddListComponent,
   ],
   templateUrl: './list-component.component.html',
   styleUrl: './list-component.component.scss'
 })
 export class ListComponentComponent {
   items = ['1','2','3','4'];
+  isVisible = false;
 }
